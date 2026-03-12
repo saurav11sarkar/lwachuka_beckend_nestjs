857e5bf53e7c3336d1284ba158ec372d9db4980c
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-03-12 19:52:07 +0600

    add swagger

--- src/app/module/advertisementmanagement/dto/update-advertisementmanagement.dto.ts
+++ src/app/module/advertisementmanagement/dto/update-advertisementmanagement.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdvertisementmanagementDto } from './create-advertisementmanagement.dto';
 
-export class UpdateAdvertisementmanagementDto extends PartialType(CreateAdvertisementmanagementDto) {}
+export class UpdateAdvertisementmanagementDto extends PartialType(
+  CreateAdvertisementmanagementDto,
+) {}
